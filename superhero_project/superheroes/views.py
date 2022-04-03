a411e036d1dbfc4c3861753664213f69b85af3bf
Cole Massey <cole.massey33@gmail.com>
2022-04-02 20:43:17 -0500

    edit delete finished and mvp reached.

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -14,11 +14,11 @@ def index(request):
     return render(request, 'superheroes/index.html', context)
 
 def detail(request, hero_id):
-    single_hero = Superhero.objects.get(pk = hero_id)
+    single_hero = Superhero.objects.get(pk=hero_id)
     context = {
-        'single_hero' : single_hero
+        'single_hero': single_hero
     }
-    return render(request, 'superheroes/detail.html', context)
+    return render(request, 'superheroes/details.html', context)
 
 def create(request):
     if request.method == 'POST':
@@ -34,3 +34,24 @@ def create(request):
         # return to index
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        Superhero.objects.filter(pk=hero_id).update(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        single_hero = Superhero.objects.get(pk=hero_id)
+        context = {
+            'single_hero': single_hero,
+            'hero_id': hero_id
+        }
+        return render(request, 'superheroes/edit.html', context)
+
+def delete(request, hero_id):
+    Superhero.objects.filter(pk=hero_id).delete()
+    return HttpResponseRedirect(reverse('superheroes:index'))
